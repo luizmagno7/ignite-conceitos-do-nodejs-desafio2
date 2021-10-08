399bb7493dd451319fbe305988b2ed7fcffd9e46
Luiz Magno <luiz.magno7@yahoo.com.br>
2021-10-08 01:58:32 -0300

    resolução do desafio 2

--- src/index.js
+++ src/index.js
@@ -10,19 +10,67 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find(( user ) => user.username == username );
+
+  if(!user) {
+    return response.status(404).json({ message: "Usuário não existe!" })
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  if (user.pro === false && user.todos.length == 10) {
+    return response.status(403).json({ message: "Usuário já atingiu o número máximo de tarefas!" });
+  }
+
+  return next();
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  let user = users.find(( user ) => user.username == username );
+
+  if (!user) {
+    return response.status(404).json({ message: "Usuário não existe!" });
+  }
+
+  if (!validate(id)) {
+    return response.status(400).json({ message: "ID inválido!" });
+  }
+
+  let todo = user.todos.find(( todo ) => todo.id = id);
+
+  if (!todo) {
+    return response.status(404).json({ error: "Tarefa não encontrada!" });
+  }
+
+  request.user = user;
+  request.todo = todo;
+
+  return next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  let user = users.find(( user ) => user.id === id);
+
+  if (!user) {
+    return response.status(404).json({ message: "Usuário não existe!" });
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
